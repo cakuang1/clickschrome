c40ace84fbe1060f0fa4bc58ece69eeb3c91c1d2
cakuang1 <cakuang1@berkeley.edu>
2023-09-20 20:38:46 -0700

    added producer

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,13 +1,24 @@
-
 import pika
+import psycopg2
+import os
 
 
+    
+postgres_connection = psycopg2.connect(
+    host='your_postgresql_host',
+    database='your_database_name',
+    user='your_username',
+    password='your_password'
+)
+
+postgres_cursor = postgres_connection.cursor()
 
 
 def callback(ch, method, properties, body):
+
+
+
     print("Received message:", body)
-
-
 
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',5672))
@@ -16,5 +27,4 @@ queue_name = 'clicks'
 channel.queue_declare(queue=queue_name)
 channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 
-print('Waiting for messages. To exit, press CTRL+C')
 channel.start_consuming()
